c23c8d85361914700f4fae906c4b189d62d576d2
Darren Shepherd <darren@rancher.com>
2015-09-17 14:40:58 -0700

    Hacks

--- handlers.go
+++ handlers.go
@@ -5,19 +5,37 @@ import (
 	"fmt"
 	"html"
 	"net/http"
+
+	"github.com/rancher/go-rancher/client"
 )
 
 func ListTemplates(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "Request URL:, %q\n", html.EscapeString(r.URL.Path))
+	//fmt.Fprintf(w, "Request URL:, %q\n", html.EscapeString(r.URL.Path))
 
 	//read the cloned repo starting at ./DATA/templates
 
-    for _, value := range Catalog{
-	  json.NewEncoder(w).Encode(value)
+	resp := TemplateCollection{}
+	for _, value := range Catalog {
+		PopulateResource(&value.Resource)
+		resp.Data = append(resp.Data, value)
 	}
+
+	PopulateCollection(&resp.Collection)
+	json.NewEncoder(w).Encode(resp)
 }
 
 func LoadTemplateDetails(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Request URL:, %q", html.EscapeString(r.URL.Path))
+}
+
+func PopulateCollection(collection *client.Collection) {
+	collection.Type = "collection"
+}
+
+func PopulateResource(r *http.Request, resourceType, resourceId string, resource *client.Resource) {
+	resource.Type = "template"
+	resource.Links = map[string]string{
+		"self": "foo",
+	}
 }
--- template.go
+++ template.go
@@ -1,6 +1,9 @@
 package main
 
+import "github.com/rancher/go-rancher/client"
+
 type Template struct {
+	client.Resource
 	Name           string `json:"name"`
 	Category       string `json:"category"`
 	Description    string `json:"description"`
